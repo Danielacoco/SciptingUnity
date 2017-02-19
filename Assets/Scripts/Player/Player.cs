0fd4c6398a508127ff7a9e6563b191ef81871268
Danielacoco <danielacoor6@gmail.com>
2017-02-18 17:04:39 -0800

    scripts for oponent and player interaction

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public int health = 3;
+
+	void collideWithOponent (Oponent oponent){
+		oponent.Attack (this);
+		if (health <= 0) {
+			//do something
+		}
+		
+		
+	}
+	void OnCollisionEnter(Collision colision){
+		Oponent op = colision.collider.gameObject.GetComponent<Oponent> (); 
+		if (op) {
+			collideWithOponent (op);
+		}
+	}
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+}
